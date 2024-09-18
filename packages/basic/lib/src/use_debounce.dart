8f1bdd7006b3a2ff4fecea54f5b8f72c43c3bd9c
Phan Minh Nguyên <nguyenphan@gmail.com>
2024-09-18 10:30:21 +0700

    feat(authentication): fix login page

--- packages/basic/lib/src/use_debounce.dart
+++ packages/basic/lib/src/use_debounce.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_use_basic/flutter_use.dart';
@@ -10,4 +12,13 @@ import 'package:flutter_use_basic/flutter_use.dart';
 void useDebounce(VoidCallback fn, Duration delay, [List<Object?>? keys]) {
   final timeout = useTimeoutFn(fn, delay);
   useEffect(() => timeout.reset, keys);
+}
+
+void Function(T)? useDebouncedTimer<T>(void Function(T) callback, Duration delay) {
+  final timer = useRef<Timer?>(null);
+
+  return (T value) {
+    timer.value?.cancel();
+    timer.value = Timer(delay, () => callback(value));
+  };
 }
